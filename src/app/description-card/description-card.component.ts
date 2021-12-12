ce40565991a72d8e2361bf201d138d5591a73af7
zlaxton <zlatan.kopic171@yahoo.com>
2021-12-12 18:44:42 +0100

    Create description-card.component.ts

---
+++ src/app/description-card/description-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description-card',
+  templateUrl: './description-card.component.html',
+  styleUrls: ['./description-card.component.scss']
+})
+export class DescriptionCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
