adbe053b01cb4c54fb59224afa66b32419451ba3
zlaxton <zlatan.kopic171@yahoo.com>
2021-12-12 18:45:32 +0100

    Create genre-card.component.ts

---
+++ src/app/genre-card/genre-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-card',
+  templateUrl: './genre-card.component.html',
+  styleUrls: ['./genre-card.component.scss']
+})
+export class GenreCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
