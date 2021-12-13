ff3ada55a090bf73a0de79f666d2ac7b7df697c6
zlaxton <zlatan.kopic171@yahoo.com>
2021-12-13 10:57:49 +0100

    Update welcome-page.component.ts

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 
 @Component({
@@ -23,6 +24,11 @@ export class WelcomePageComponent implements OnInit {
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
+    });
+  }
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
     });
   }
 }
